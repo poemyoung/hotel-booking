72876dd8ae583153fabe5d09a3fa7507e925a5e0
poemyoung <2427389569@qq.com>
2020-06-28 16:56:58 +0800

    HotelBookingApplication

--- src/main/java/com/wxwl/hotelbooking/HotelBookingApplication.java
+++ src/main/java/com/wxwl/hotelbooking/HotelBookingApplication.java
@@ -1,12 +1,9 @@
 package com.wxwl.hotelbooking;
 
-import com.wxwl.hotelbooking.common.jwt.JwtAuthenticationFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.wxwl.hotelbooking")
